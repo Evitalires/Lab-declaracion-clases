2c202e36ac093e355bf63255c3a9143870c0f55c
Evitalires <evitalires@gmail.com>
2025-11-04 14:22:09 -0500

    Finalizado el ejercicio 3

--- ejercicios.js
+++ ejercicios.js
@@ -57,7 +57,7 @@ class Empleado {
     }
     calcularSalario(){
         console.log(
-            `El salario base de ${this.nombre}, es: $${this.salarioBase}`
+            `El salario de ${this.nombre}, es: $${this.salarioBase}`
         );
     }
 }
@@ -72,11 +72,80 @@ class Gerente extends Empleado {
     calcularSalario(){
         console.log(
             `El Gerente ${this.nombre}. tiene:
-            Un salario base de $${this.salarioBase}. 
-            Una bonificación de $${this.bonificacion}
+            Un salario base de $ ${this.salarioBase}. 
+            Una bonificación de $ ${this.bonificacion}
             El salario final del Gerente es: $ ${this.salarioBase + this.bonificacion}`
         );
     }
 }
 const testGerente = new Gerente("Javier", 35, 25000, 5000);
 testGerente.calcularSalario()
+
+class Desarrolador extends Empleado {
+    constructor(nombre, edad, salarioBase, lenguaje) {
+        super(nombre, edad, salarioBase);
+        this.lenguaje = lenguaje;
+    }
+    mostrarLeguaje(){
+        console.log(`El lenguaje del desarrollado es ${this.lenguaje}`);
+    }
+}
+const testDesarrollador = new Desarrolador("Jair", 35, 19000, "JavaScript");
+testDesarrollador.calcularSalario();
+testDesarrollador.mostrarLeguaje();
+
+
+//Ejercicio 3: Sistema de productos electrónicos
+class Producto {
+    constructor(nombre, precio, marca) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+        this.descripcion = `Producto ${this.nombre}
+        Marca: ${this.marca}
+        Precio: $${this.precio}`
+    }
+    darDescripcion(){
+        console.log(this.descripcion);
+    }
+}
+const testProducto = new Producto("Coca Cola 500ml", 3000, "Cocacola" );
+testProducto.darDescripcion();   
+
+class Telefono extends Producto {
+    constructor(nombre, precio, marca, procesador, pulgadas) {
+        super(nombre, precio, marca);
+        this.procesador = procesador;
+        this.pulgadas = pulgadas;
+        this.descripcionExtendida = this.descripcion + ` 
+        Procesador: ${this.procesador}
+        pulgadas: ${this.pulgadas}
+        ` 
+    }
+    darDescripcion() {
+        console.log(this.descripcionExtendida);
+    }
+
+}
+
+const testTelefono = new Telefono("Pocophone x6 pro", 600000, "Xiaomi", "500gb", "12GB" );
+testTelefono.darDescripcion();   
+
+class Laptop extends Producto {
+    constructor(nombre, precio, marca, procesador, pulgadas) {
+        super(nombre, precio, marca);
+        this.procesador = procesador;
+        this.pulgadas = pulgadas;
+        this.descripcionExtendida = this.descripcion + ` 
+        Almacenimiento: ${this.procesador}
+        Pulgadas: ${this.pulgadas}
+        ` 
+    }
+    darDescripcion() {
+        console.log(this.descripcionExtendida);
+    }
+
+}
+
+const testLaptop = new Laptop("Macbook Pro", 800000, "Apple", "Apple Core 5", "17" );
+testLaptop.darDescripcion();   
